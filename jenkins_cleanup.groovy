d5e7ad62fb6afa7e8fb6f11cd886d96a0c48f71a
SandhyaRajarathnam <32507547+SandhyaRajarathnam@users.noreply.github.com>
2021-07-21 18:54:22 +0530

    Updating jenkins_clanup.groovy to cleanup only artifacts of builds older than 10 days

--- jenkins_cleanup.groovy
+++ jenkins_cleanup.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.Jenkins
 import hudson.model.Job
 
-BUILDS_TO_KEEP = 0
+    BUILDS_TO_KEEP = 0
     def tenDaysAgo=new Date() -10
     Jenkins.instance.getAllItems(Job.class).each { job->
     println job.name
@@ -10,12 +10,19 @@ BUILDS_TO_KEEP = 0
     !recent.contains(it) && ! (it.getTime() > tenDaysAgo)
   }
   if(!buildsToDelete){
-      println "No build older than 10 days"
+      println "No Build older than 10 days"
   }
     for(build in buildsToDelete){
         if(!recent.contains(build)){
-            println "Going to delete : " + build
-           build.delete()
+            def artifacts=build.artifacts
+            if(artifacts){
+                println "Deleting : " + artifacts
+                build.deleteArtifacts()
+           }
+           else{
+               println "No artifacts present"
+           }
+           
         }
     }
 }
